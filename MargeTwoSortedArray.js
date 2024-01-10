25c0a87704ca821608f92613ca118ef40f1093c3
Vipin mavi <vipinmavi@vinay-MacBook-Pro.local>
2024-01-10 11:42:03 +0530

    marge two sorted array

--- MargeTwoSortedArray.js
+++ MargeTwoSortedArray.js
@@ -1,23 +1,31 @@
 function mergeArray(array1, array2) {
     let mergedArray = array2;
     array1.map(( element, i ) => {
-        mergeArray.push(element);
+        mergedArray.push(element);
     })
     return mergedArray;
 }
-function MTSA(params) {
+function MTSA(array1, array2) {
     let TempSmallestElement;
     let sortedArray = [];
-    let mergedArray = mergeArray([2,5,6,9], [1,2,3,29]);
-    for (let index = 0; mergedArray.length != 0; index++) {
-        TempSmallestElement = mergeArray[0];
-        if(TempSmallestElement <= mergedArray[index]){
+    let mergedArray = mergeArray(array1, array2);
 
-        }else{
+    for (let parIndex = 0; mergedArray.length != 0; ) {
+        TempSmallestElement = mergedArray[parIndex];
+        for (let index = 0; index < mergedArray.length; index++) {
 
+            if(TempSmallestElement > mergedArray[index]){
+                TempSmallestElement = mergedArray[index];
+            }
+            if(index == mergedArray.length-1){
+                sortedArray.push(TempSmallestElement);
+                let elementIndex = mergedArray.indexOf(TempSmallestElement);
+                mergedArray.splice(elementIndex, 1);
+            }
+            if(index==100) return;
         }
     }
-
+    return sortedArray;
 }
-let finalArray = MTSA([2,5,6,9], [1,2,3,29]);
+let finalArray = MTSA([2,5,6,9], [1,2,31,2,9]);
 console.log("Result : " + finalArray);
